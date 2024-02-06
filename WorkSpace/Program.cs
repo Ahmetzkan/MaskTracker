23804cd55a50dcf3e39d88cfa7f07358281b82f2
Ahmet Özkan <70511583+Ahmetzkan@users.noreply.github.com>
2024-02-06 14:57:34 +0300

    Add project files.

---
+++ WorkSpace/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person person = new Person();
+person.FirstName = "Ahmet";
+person.LastName = "Özkan";
+person.DateOfBirthYear = 1999;
+person.NationalIdentity = 123;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person);
+
+
+static void Variables()
+{
+    string message = "Merhaba";
+    double total = 100000;
+    int number = 100;
+    bool isItLogIn = false;
+    Console.WriteLine(total * 1.18);
+    Console.WriteLine(total * 1.18);
+}
